d96fde34248695269f193767f3595f92664e9a99
karolsek12 <karol.sek12@wp.pl>
2026-02-07 13:09:05 +0100

    chapter 5

--- Ray.cs
+++ Ray.cs
@@ -29,8 +29,25 @@ namespace RayTracing
             this.direction = direction;
         }
 
+        public bool hitSphere(Point3 center, double r)
+        {
+            Vec3 oc = center - origin;
+            double a = Vec3.dot(direction, direction);
+            double b = -2.0 * Vec3.dot(direction, oc);
+            double c = Vec3.dot(oc, oc) - r * r;
+
+            double delta = b * b - 4 * a * c;
+
+            return delta >= 0;
+        }
+
         public Color3 rayColor()
         {
+            if(hitSphere(new Point3(0, 0, -1), 0.5))
+            {
+                return new Color3(1, 0, 0);
+            }
+
             Vec3 unitDirection = Vec3.unitVector(direction);
             double a = 0.5 * (unitDirection.y + 1.0);
 
